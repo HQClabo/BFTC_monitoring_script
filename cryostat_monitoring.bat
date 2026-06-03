be0a92452ea2c54f8ad392a636d8126ef6797d18
HQClabo <80475383+HQClabo@users.noreply.github.com>
2026-06-03 16:51:31 +0200

    adjust default batch file

--- cryostat_monitoring.bat
+++ cryostat_monitoring.bat
@@ -1,5 +1,5 @@
 cd C:\Users\hqclabo\Documents\Python_Scripts\BFTC_monitoring_script
-call C:\Users\hqclabo\Anaconda3\Scripts\activate.bat base
+call C:\Users\hqclabo\AppData\Local\anaconda3\Scripts\activate.bat base
 :start
-call cryostat_monitoring.py
+python cryostat_monitoring.py
 goto start
